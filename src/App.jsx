4f21aed4a354d891f8f235473130230bdb87ad78
Thomas Somers III <tksiii@Thomass-Mac-mini.local>
2026-07-17 11:08:30 -0500

    Create initial Insight BI homepage

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div>
+      <h1>Insight BI</h1>
+
+      <p>Built by Thomas Somers</p>
+
+      <button>Get Started</button>
+
+      <button>Learn More</button>
+    </div>
+  );
+}
+
+export default App;
